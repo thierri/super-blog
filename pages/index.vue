fd509e6929b5975d8e1f9c14ee4e88fb3e0d5153
Thierri Oliveira <thierri@bitfix.com.br>
2021-03-24 12:54:26 -0300

    Step

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <div>
+      <h1>Titulo do Blog</h1>
+      <article>
+          <h2>Titulo do Artigo</h2>
+          <p>Resumo do Artigo</p>
+          <p>Ler Mais</p>
+      </article>
+  </div>
 </template>
 
 <script>
