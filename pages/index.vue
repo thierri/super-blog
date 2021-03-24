ecbcdd20f3ef1b3144ea058cafa96c43783d63fd
Thierri Oliveira <thierri@bitfix.com.br>
2021-03-24 13:31:20 -0300

    step

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="w-1/2 mx-auto my-20">
-      <h1 class="text-3xl font-semibold text-red-600">Titulo do Blog</h1>
+      <h1 class="text-3xl font-semibold text-red-600">
+          {{tituloBlog}}
+      </h1>
       <article class="my-20 text-gray-600">
           <h2 class="text-5xl font-semibold mb-5">Titulo do Artigo</h2>
           <p class="mb-5">Resumo do Artigo</p>
@@ -10,5 +12,11 @@
 </template>
 
 <script>
-export default {}
+export default {
+    data(){
+        return {
+            tituloBlog: "Super Blog"
+        }
+    } 
+}
 </script>
